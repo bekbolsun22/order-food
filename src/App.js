37f60dce8c213be6f8dea33c46e4f316bf17425e
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2023-07-28 13:04:06 +0600

    add meals summary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from './components/header/Header'
+import { MealsSummary } from './components/meals-summary/MealsSummary'
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<MealsSummary />
 			{/* <Header />
 			<MealsSummary />
 			<Meals />
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
 export { ReactComponent as BasketIcon } from './icons/basket-icon.svg'
+
+export { default as MealsBannerImage } from './images/meals-image.png'
